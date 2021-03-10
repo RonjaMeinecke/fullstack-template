31cfda82fa6f0cf9ea8436f18e6455a4540c2054
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-03-10 15:25:08 +0100

    ➕ add ESLint
    https://eslint.org/docs/user-guide/getting-started

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
+import type { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
